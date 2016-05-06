76d595887b246c04b37d1e27c094369810584815
bbrooker <latte.book@icloud.com>
2016-05-06 22:00:27 +0700

    Minespace Core
    Minespace Core Plugin contained necessary command for admin and player

---
+++ net/bbrooker/minespace/Minespace.java
@@ -0,0 +1,24 @@
+package net.bbrooker.minespace;
+
+
+
+public class Minespace extends JavaPlugin {
+
+public void onEnable() {
+
+getLogger().Info("");
+
+}
+
+public void onDisable() {
+
+getLogger().Info("");
+
+}
+
+@EventHandler
+
+
+
+
+}
