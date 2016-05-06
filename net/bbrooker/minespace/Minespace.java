a6a1815c27e7af5f777398fa622e3a9bddc2bad0
lookmaiarin <lukmai1398@gmail.com>
2016-05-06 22:19:31 +0700

    Update Minespace.java

--- net/bbrooker/minespace/Minespace.java
+++ net/bbrooker/minespace/Minespace.java
@@ -4,10 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MineSpace extends JavaPlugin implements Listener {
+public class MineSpace extends JavaPlugin {
 
 	
 	public void onEnable()
